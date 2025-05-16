c021df18429c705c2c8d99209b7d926154ce354f
Raghad Thawabi <raghadmoh.tha@gmail.com>
2025-05-16 14:23:55 +0300

    campaignService modify

--- services/campaignService.js
+++ services/campaignService.js
@@ -40,39 +40,3 @@ exports.donateToCampaign = async ({ campaign_id, user_id, amount }) => {
 const campaignRepo = require('../repositories/campaignRepository');
 const { sendEmail } = require('../utils/sendEmail');
 
-exports.createCampaign = async (data) => {
-
-
-  return await campaignRepo.insertCampaign(data);
-};
-
-exports.notifyDonors = async (title, description, location) => {
-  const donors = await campaignRepo.getDonorEmails();
-
-  for (const donor of donors) {
-    const mapUrl = location
-      ? `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(location)}`
-      : 'https://hopeconnect.org/campaigns';
-
-    await sendEmail({
-      to: donor.email,
-      subject: `🚨 Emergency Campaign: ${title}`,
-      html: `
-        <p>Dear ${donor.name},</p>
-        <p>We need your urgent support for: <strong>${title}</strong>.</p>
-        <p>${description}</p>
-        ${location ? `<p>📍 Location: <a href="${mapUrl}">${location}</a></p>` : ''}
-        <p><a href="https://hopeconnect.org/campaigns">Click here to help</a></p>
-        <p>- HopeConnect Emergency Team</p>
-      `
-    });
-  }
-};
-
-exports.getActiveCampaigns = async () => {
-  return await campaignRepo.getActiveCampaigns();
-};
-
-exports.donateToCampaign = async ({ campaign_id, user_id, amount }) => {
-  return await campaignRepo.insertCampaignDonation({ campaign_id, user_id, amount });
-};
